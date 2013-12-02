9ce8eb8dd220c79324091294857b9ad5ca59121e
asimon <alejandro@kano.me>
2013-12-02 20:20:14 +0000

    Added License and bumped version

--- cpp/mainwindow.cpp
+++ cpp/mainwindow.cpp
@@ -1,7 +1,9 @@
 /****************************************************************************
-**
-** A tool for sharing feedback on the Kano OS
-**
+# main.cpp
+#
+# Copyright (C) 2013 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
 ****************************************************************************/
 
 #include <QAction>
