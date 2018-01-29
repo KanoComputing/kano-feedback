d4101b8123eef3b5a5612d4cfafb0d5762578f01
Radu Jipa <radu.t.jipa@gmail.com>
2018-01-29 13:57:59 +0000

    Refactored kano-feedback-cli executable interface with more options
    This adds more control over the parameters essentially passed to
    `send_data` and others. It also makes it clearer what exit code
    it'll have.

---
+++ kano_feedback/kano_feedback_cli.py
@@ -0,0 +1,49 @@
+# kano_feedback_cli.py
+#
+# Copyright (C) 2018 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
+#
+# The main functionality of the kano-feedback-cli binary.
+
+
+from kano_feedback.DataSender import send_data, take_screenshot, \
+    copy_archive_report, delete_tmp_dir
+from kano_feedback.paths import Path
+from kano_feedback.return_codes import RC
+
+
+def main(args):
+    """The main functionality of the kano-feedback-cli binary.
+
+    Returns:
+        int: Exit code as documented in :class:`.return_codes.RC`
+    """
+
+    report_file = args['--output'] or Path.DEFAULT_REPORT_PATH
+
+    if args['--screenshot']:
+        print 'Taking a screenshot...'
+        take_screenshot()
+
+    print 'Gathering feedback data...'
+    successful, error = send_data(
+        args['--description'] or '',
+        True,
+        subject=args['--title'] or 'Kano OS: Feedback Report Logs',
+        network_send=args['--send']
+    )
+    if not successful:
+        print 'Error from send_data: {}'.format(error)
+        return RC.ERROR_SEND_DATA
+
+    successful = copy_archive_report(report_file)
+    delete_tmp_dir()
+
+    if successful:
+        print 'Feedback file saved to:', report_file
+    else:
+        print 'Could not save feedback to:', report_file, \
+              'Perhaps you need to use sudo?'
+        return RC.ERROR_COPY_ARCHIVE
+
+    return RC.SUCCESS
