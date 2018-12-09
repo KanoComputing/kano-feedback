db70b8a3b07a9211f5f6f640e19c5ad66f1c2e24
Tom Bettany <tom@kano.me>
2018-12-09 14:19:33 +0000

    Logs: Add apt source files
    Include the apt source files in the collected logs.

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+#
+# conftest.py
+#
+# Copyright (C) 2018 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
+#
+# The pytest conftest file
+#
+# Import fixtures and setup the tests
+#
+
+
+from tests.fixtures.apt_sources import *
+from tests.fixtures.console_mode import *
