d96e9ab205a182b52bd2ca65d0aea1589e5bd177
Tom Bettany <tom@kano.me>
2019-01-08 16:26:16 +0000

    Logs: Add `apt` and `dpkg` install logs
    Add the `apt` and `dpkg` install logs to the list of files to be
    included in the submitted logs.

--- tests/fixtures/apt_sources.py
+++ tests/fixtures/apt_sources.py
@@ -1,7 +1,7 @@
 #
 # apt_sources.py
 #
-# Copyright (C) 2018 Kano Computing Ltd.
+# Copyright (C) 2018-2019 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
 # Test fixtures to simulate apt sources files
@@ -9,66 +9,29 @@
 
 
 import os
-import math
-import random
-import string
 import pytest
 
-
-#
-# Generate the set of all subsets
-#
-# Special thanks to:
-#     https://stackoverflow.com/questions/1482308/how-to-get-all-subsets-of-a-set-powerset#answer-1482359
-#
-def powerset(lst):
-    return reduce(
-        lambda result, x: result + [subset + [x] for subset in result],
-        lst,
-        [[]]
-    )
+from tests.fixtures.helpers import powerset, FakeFile
 
 
-def random_string(length):
-    char_selection = string.letters + ' \n'
-
-    # random.sample fails if the length required is greater than the population
-    factor = int(math.ceil(length / len(char_selection)))
-    char_selection *= factor * 3
-
-    return ''.join(
-        random.sample(char_selection, length)
-    )
-
-
-def random_file():
-    return random_string(random.randint(100, 200))
-
-
-class AptSrcFile(object):
-    def __init__(self, filepath, contents):
-        print contents
-        self.filepath = filepath
-        self.contents = contents
-
+class AptSrcFile(FakeFile):
     def __repr__(self):
         return 'AptSrcFile({})'.format(self.filepath)
-
-    def __gt__(self, other):
-        return self.filepath > other.filepath
 
 
 APT_DIR = '/etc/apt'
 APT_SRC_DIR = os.path.join(APT_DIR, 'sources.list.d')
 
 APT_SRC_FILES = [
-    AptSrcFile(os.path.join(APT_DIR, 'sources.list'), random_file()),
-    AptSrcFile(os.path.join(APT_SRC_DIR, 'kano.list'), random_file()),
-    AptSrcFile(os.path.join(APT_SRC_DIR, 'collabora.list'), random_file()),
+    AptSrcFile(os.path.join(APT_DIR, 'sources.list')),
+    AptSrcFile(os.path.join(APT_SRC_DIR, 'kano.list')),
+    AptSrcFile(os.path.join(APT_SRC_DIR, 'collabora.list')),
 ]
 
 
-@pytest.fixture(scope='function', params=powerset(APT_SRC_FILES))
+@pytest.fixture(scope='function', params=powerset(APT_SRC_FILES),
+                ids=[str(log) for log in powerset(APT_SRC_FILES)]
+)
 def apt_sources(fs, request):
     sources = sorted(request.param)
 
